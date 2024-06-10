a132fe8ee1496480624a6eaaa28d901f8d61743b
HernanGoldy <hernanmdq@gmail.com>
2024-06-10 10:11:15 -0300

    Inyecto servicio en el controlador

--- src/main/java/com/bit/presentation/controller/ClientController.java
+++ src/main/java/com/bit/presentation/controller/ClientController.java
@@ -1,6 +1,9 @@
-package com.bit.presentation.dto.controller;
+package com.bit.presentation.controller;
 
 import com.bit.presentation.dto.ClientDTO;
+import com.bit.service.interfaces.IClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,33 +13,36 @@ import java.util.List;
 @RequestMapping("/client")
 public class ClientController {
 
+    @Autowired
+    private IClientService clientService;
+
     // Find all clients
     @GetMapping("/find")
     public ResponseEntity<List<ClientDTO>> findAll() {
-        return null;
+        return new ResponseEntity<>(this.clientService.findAll(), HttpStatus.OK);
     }
 
     // Find client by id
     @GetMapping("/find/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
-        return null;
+        return new ResponseEntity<>(this.clientService.findById(id), HttpStatus.OK);
     }
 
     // Create client
     @PostMapping("/create")
     public ResponseEntity<ClientDTO> createClient(@RequestBody ClientDTO clientDTO) {
-        return null;
+        return new ResponseEntity<>(this.clientService.createClient(clientDTO), HttpStatus.CREATED);
     }
 
     // Update client
     @PutMapping("/update/{id}")
     public ResponseEntity<ClientDTO> updateClient(@RequestBody ClientDTO clientDTO, @PathVariable Long id) {
-        return null;
+        return new ResponseEntity<>(this.clientService.updateClient(clientDTO, id), HttpStatus.OK);
     }
 
     // Delete client
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteClient(@PathVariable Long id) {
-        return null;
+        return new ResponseEntity<>(this.clientService.deleteClient(id), HttpStatus.NO_CONTENT);
     }
 }
