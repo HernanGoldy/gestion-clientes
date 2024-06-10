7b48d11bc3519c0f83b06b04174a779d1727da7e
HernanGoldy <hernanmdq@gmail.com>
2024-06-10 09:20:35 -0300

    inicio del proyecto CRUD

---
+++ src/main/java/com/bit/GcV5Application.java
@@ -0,0 +1,13 @@
+package com.bit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcV5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcV5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bit/GcV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
